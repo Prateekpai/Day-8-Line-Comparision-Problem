7b415dd5ca9eaf84f6f0400a646b9d726a98b54e
Prateekpai <prateekvasanthpai@gmail.com>
2022-03-29 07:57:46 +0530

    CompareTo Method

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LCP
+{
+    public  class Program
+    {
+        public static void Main (string[]args)
+        {
+            UC4 uc4 = new UC4();
+            uc4.Problrm3();
+        }
+    }
+}
